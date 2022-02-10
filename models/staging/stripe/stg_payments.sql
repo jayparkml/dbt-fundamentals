78c72e0fae800c9f6ab9cae9441add8db7ac07e4
Sungju Park <jay.park@missionlane.com>
2022-02-10 22:45:32 +0000

    changes

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,15 @@
+WITH payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount,
+        created
+
+    from raw.stripe.payment
+
+)
+
+select * from payments
